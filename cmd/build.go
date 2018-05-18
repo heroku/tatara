c2338d1bb49b948a109cbd838003ba7ca64fa715
Joe Kutner <jpkutner@gmail.com>
2018-05-18 13:48:23 -0500

    rename slug file, and remove proxy support

--- cmd/build.go
+++ cmd/build.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 	"path/filepath"
 	"errors"
 
@@ -32,32 +30,20 @@ var cmdBuild = cli.Command{
 		appDir := filepath.Clean(c.Args[0])
 		appName := filepath.Clean(c.Args[1])
 
-    	// TODO put in setup (this will be reused in run)
-		proxy := engine.ProxyConfig{
-			HTTPProxy:  firstEnv("HTTP_PROXY", "http_proxy"),
-			HTTPSProxy: firstEnv("HTTPS_PROXY", "https_proxy"),
-			NoProxy:    firstEnv("NO_PROXY", "no_proxy"),
-		}
-		if useProxy, ok := boolEnv("CFL_USE_PROXY"); ok {
-			if useProxy {
-				proxy.UseRemotely = true
-			} else {
-				proxy = engine.ProxyConfig{}
-			}
-		}
+		// TODO add options for:
+		//  - BuildStack name
+		//  - buildpack URLs/names
+		//  - SkipStackPull (default false)
 
-		engine, err := docker.New(&engine.EngineConfig{
-			Proxy: proxy,
-		})
+		engine, err := docker.New(&engine.EngineConfig{})
 		if err != nil {
 			return cli.ExitStatusUnknownError, err
 		}
 		defer engine.Close()
 
-		// begin build specific code
 		stager := forge.NewStager(engine)
 
-		dropletPath := fmt.Sprintf("./%s.droplet", appName)
+		slugPath := fmt.Sprintf("./%s.slug", appName)
 		cachePath := fmt.Sprintf("./.%s.cache", appName)
 
 		appTar, err := app.Tar(appDir)
@@ -78,7 +64,7 @@ var cmdBuild = cli.Command{
 			Buildpack: "",
 		}
 
-		droplet, err := stager.Stage(&forge.StageConfig{
+		slug, err := stager.Stage(&forge.StageConfig{
 			AppTar:        appTar,
 			Cache:         cache,
 			CacheEmpty:    cacheSize == 0,
@@ -87,41 +73,21 @@ var cmdBuild = cli.Command{
 			ForceDetect:   true,
 			Color:         color.GreenString,
 			AppConfig:     app,
+			OutputFile:    "slug.tgz",
+			SkipStackPull: true,
 		})
 		if err != nil {
 			return cli.ExitStatusUnknownError, err
 		}
-		defer droplet.Close()
+		defer slug.Close()
 
-		if err := streamOut(*sysFS, droplet, dropletPath); err != nil {
+		if err := streamOut(*sysFS, slug, slugPath); err != nil {
 			return cli.ExitStatusUnknownError, err
 		}
 
 		return cli.ExitStatusSuccess, nil
 	},
 }
-
-// TODO put in utils
-
-func firstEnv(ks ...string) string {
-	for _, k := range ks {
-		if v := os.Getenv(k); v != "" {
-			return v
-		}
-	}
-	return ""
-}
-
-func boolEnv(k string) (v, ok bool) {
-	switch strings.TrimSpace(strings.ToLower(os.Getenv(k))) {
-	case "true", "yes", "1":
-		return true, true
-	case "false", "no", "0":
-		return false, true
-	}
-	return false, false
-}
-
 
 func streamOut(fs fs.FS, stream engine.Stream, path string) error {
 	file, err := fs.WriteFile(path)
